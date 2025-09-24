6e6651fd787857e8b27fc82475789a4e8dbff10e
Артём <dhdb82@inbox.ru>
2025-09-24 11:04:44 +0300

    add pages

---
+++ joyce_front/src/Pages/Catalog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../components/UI/Header";
+
+function Catalog() {
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    )
+}
+
+export default Catalog;
